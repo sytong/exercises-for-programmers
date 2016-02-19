fae608f478a188c15f4eb5c974ea6ac8c580b791
Willie Tong <tongsinyin@gmail.com>
2016-02-19 17:33:35 +0800

    bug fix

--- 49-flickr-photo-search/jquery/js/app.js
+++ 49-flickr-photo-search/jquery/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var $tagMode = $('#search-panel > .selectpicker');
   var $search = $('#search');
 
-  var flickr = function(tags, tagMode, loadImage) {
+  var searchAll = function(tags, tagMode, fn) {
     $.ajax({
       url: url,
       data: {
@@ -14,7 +14,7 @@ $(document).ready(function(){
         format: "json"
       },
       dataType: 'jsonp',
-      success: addImages,
+      success: fn,
       error: function(response, status) {
         console.log(response);
       }
@@ -28,12 +28,32 @@ $(document).ready(function(){
       for (var j=0; j<4; j++) {
         var $col = $('<div class="col-lg-3">').appendTo($row);
         var $a = $('<a href="#" class="thumbnail">').appendTo($col);
-        $a.append($('<img>').attr('src', data.items[i * 4 + j].media.m))
+        var photo = data.items[i * 4 + j];
+        $a.append($('<img>').attr('src', photo.media.m));
+
+        $a.data('title', photo.title);
+        $a.data('author', photo.author);
+        $a.data('published', photo.published);
+        $a.data('tags', photo.tags);
+        $a.data('img', photo.media.m);
       }
     }
+
+    $('a.thumbnail').on('click', function() {
+      var $a = $(this);
+      var $img = $('<img>').attr('src', $a.data('img'));
+      var $details = $('#modal-photo-details');
+
+      $('.modal-title').text($a.data('title'));
+      $('#modal-thumbnail').empty().append($img);
+      $details.find('div:eq(0)').text($a.data('author'));
+      $details.find('div:eq(1)').text($a.data('published'));
+      $details.find('div:eq(2)').text($a.data('tags'));
+      $('#photo-details').modal('show');
+    });
   };
 
   $search.on('click', function(){
-    flickr($tags.val(), $tagMode.val(), addImages);
+    searchAll($tags.val().split(/\s+/).join(","), $tagMode.val(), addImages);
   });
 });
